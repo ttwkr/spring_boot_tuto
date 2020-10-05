f4a7e3b7d9f605a81ce4225f62b9b5694c787fe2
ttwkr <ttwkor12@gmail.com>
2020-10-05 23:06:31 +0900

    wip

--- src/main/java/com/ttwkr/webservice/ScoreController.java
+++ src/main/java/com/ttwkr/webservice/ScoreController.java
@@ -2,14 +2,14 @@ package com.ttwkr.webservice;
 
 import com.ttwkr.webservice.domain.score.ScoreRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
 public class ScoreController {
 
-    @GetMapping("/score")
+
+    @RequestMapping(value = "/score", method = RequestMethod.GET)
     public String Score(){
         System.out.println("request");
         return "SCORE!!!";
